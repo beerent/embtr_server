7a437f7c147acac167e015b322f999bfe0c1bf1f
beerent <brent@embtr.com>
2023-05-11 06:23:33 -0400

    adding cache size log

--- src/general/auth/TokenCache.ts
+++ src/general/auth/TokenCache.ts
@@ -7,6 +7,8 @@ export class TokenCache {
     public static async getDecodedToken(
         authorizationHeader: string
     ): Promise<DecodedIdToken | undefined> {
+        console.log('token cache size: ' + this.tokenCache.size);
+
         const encodedToken = this.getBearerToken(authorizationHeader);
         if (!encodedToken) {
             return undefined;
