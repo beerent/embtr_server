0e565aa89271fb93fa110cb0328e5c170b1fabd9
thedevdad <brent@embtr.com>
2024-01-15 06:41:15 -0500

    EMBTR-21 - fixed unit response type

--- src/endpoints/UnitRouter.ts
+++ src/endpoints/UnitRouter.ts
@@ -3,6 +3,8 @@ import { authenticate } from '@src/middleware/authentication';
 import { authorize } from '@src/middleware/general/GeneralAuthorization';
 import { UnitService } from '@src/service/UnitService';
 import { ContextService } from '@src/service/ContextService';
+import { GetUnitsResponse } from '@resources/types/requests/UnitTypes';
+import { SUCCESS } from '@src/common/RequestResponses';
 
 const unitRouter = express.Router();
 
@@ -10,7 +12,11 @@ unitRouter.get(['/', '/v1/'], authenticate, authorize, async (req, res) => {
     const context = await ContextService.get(req);
 
     const units = await UnitService.getAll(context);
-    res.json(units);
+    const response: GetUnitsResponse = {
+        ...SUCCESS,
+        units,
+    };
+    res.json(response);
 });
 
 export default unitRouter;
