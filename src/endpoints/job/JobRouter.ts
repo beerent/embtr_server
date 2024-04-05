afdc73569e81cc958c51190ce0dcfd50bbabbcd5
thedevdad <brent@embtr.com>
2024-04-05 12:45:12 -0400

    adding default job endpoint

--- src/endpoints/job/JobRouter.ts
+++ src/endpoints/job/JobRouter.ts
@@ -4,5 +4,6 @@ import jobRouterLatest from './JobRouterLatest';
 const jobRouter = express.Router();
 
 jobRouter.use('/:version/job', jobRouterLatest);
+jobRouter.use('/job', jobRouterLatest);
 
 export default jobRouter;
